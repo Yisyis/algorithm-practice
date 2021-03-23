04add4467b01448b571becc90ac418d772ad9849
yeshuai@netposa.com <yeshuai1223@163.com>
2021-03-23 18:29:59 +0800

    删除链表的倒数第N个节点

--- src/main/java/com/yis/linkedList/ListNode.java
+++ src/main/java/com/yis/linkedList/ListNode.java
@@ -2,6 +2,7 @@ package com.yis.linkedList;
 
 /**
  * 链表节点
+ *
  * @author YeShuai
  * @date 2021/3/22
  */
@@ -10,8 +11,16 @@ public class ListNode {
     int val;
     ListNode next;
 
+    ListNode() {
+    }
+
     ListNode(int x) {
         val = x;
         next = null;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
     }
 }
